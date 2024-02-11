9ead5ed66382f35c61abe4c2a09e141525a0dddb
Jiayan Luo <jiayanluo2024@u.northwestern.edu>
2024-02-11 03:26:26 -0600

    added reordering with seq num and receive buffer

--- streamer.py
+++ streamer.py
@@ -2,6 +2,7 @@
 from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
+import struct
 
 
 class Streamer:
@@ -13,14 +14,26 @@ class Streamer:
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+        self.format = 'i'
+        self.recvbuffer = {}
+        self.ack = 0
+        self.sequence_number = 0
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
-        # Your code goes here!  The code below should be changed!
         # Support sending data larger than 1472 bytes by breaiking data_bytes into chunks 
         bytes_list = []
-        for i in range(0, len(data_bytes), 1472):
-            bytes_list.append(data_bytes[i:i+1472])
+        sequence_number = 0
+        for i in range(0, len(data_bytes), 1468):
+            # Old approach
+            # bytes_list.append(data_bytes[i:i + 1472])
+            # Chunk data and add sequence number in header
+            data_byte = data_bytes[i:i+1468]
+            header = struct.pack(self.format, self.sequence_number) 
+            packet = header + data_byte
+            bytes_list.append(packet)
+            print(f"sequence number: {self.sequence_number}, data: {data_byte}")
+            self.sequence_number += 1
 
         # for now I'm just sending the raw application-level data in one UDP payload
         for data_byte in bytes_list:
@@ -28,12 +41,35 @@ class Streamer:
 
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
-        # your code goes here!  The code below should be changed!
-        
-        # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        # For now, I'll just pass the full UDP payload to the app
-        return data
+        # parse the sequence number and wait to return packets in order
+        # check each segment by comparing it to next sequence number expected (ACK)
+        # if sequence number received != that number, store in buffer and continue waiting
+        # if sequence number you expected is received or in your buffer, return that data
+        # busy wait with while True
+        # Inside the loop, every time you receive a packet from recvfrom, store it in the receive buffer
+        # if a packet is received that mathces ACK number, return data from recv, exiting the loop
+        while True: 
+            if self.ack in self.recvbuffer:
+                print(f"ack found!: {self.ack}")
+                packet = self.recvbuffer[self.ack]
+                del self.recvbuffer[self.ack]
+                self.ack += 1
+                return packet
+            # this sample code just calls the recvfrom method on the LossySocket
+            data, addr = self.socket.recvfrom()
+            header_length = struct.calcsize(self.format)
+            sequence_number = struct.unpack(self.format, data[:header_length])[0]
+            data_byte = data[header_length:]
+            self.recvbuffer[int(sequence_number)] = data_byte
+
+    def listener(self):
+        while not self.closed:
+            try:
+                data, addr = self.socket.recvfrom()
+                # store data in the receive buffer
+            except Exception as e:
+                print("Listener died!")
+                print(e)
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
